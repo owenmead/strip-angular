c2f7a2b7a9c0d3d095c115f4635ed060af914b64
owenmead <owenmead@gmail.com>
2014-07-16 19:38:11 -0700

    Add some basic angular pieces to our app

---
+++ app.js
@@ -0,0 +1,19 @@
+angular.module('app', [])
+	.controller('ctrl', function($scope, dataz) {
+		$scope.stuff = dataz;
+	})
+	.provider('dataz', function() {
+		return {
+			$get: function() {return ['foo', 'bar', 'baz']}
+		}
+	})
+	.directive('blink', function($interval) {
+		return {
+			restrict: 'E',
+			link: function(scope, element, attrs) {
+				$interval(function() {
+					element[0].style.display = element[0].style.display == 'none' ? 'inline' : 'none';
+				}, 500);
+			}
+		}
+	})
